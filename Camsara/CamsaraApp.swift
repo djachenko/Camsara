1fe8c25c1d4e6d404bc083250ed590db3f13e49d
Igor Djachenko <i.s.djachenko@gmail.com>
2025-12-11 23:11:48 +0700

    Initial Commit

---
+++ Camsara/CamsaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamsaraApp.swift
+//  Camsara
+//
+//  Created by justin on 11.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CamsaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
